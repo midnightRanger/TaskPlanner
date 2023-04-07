e95b5194a448b837d306f4a2f95cf1a666ac5ba3
Lev Ostapenko <012938576a@gmail.com>
2023-04-07 23:04:15 +0300

    added model

--- TaskPlannerProject/Controllers/AuthenticateController.cs
+++ TaskPlannerProject/Controllers/AuthenticateController.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using TaskPlannerProject.Models;
+using TaskPlannerProject.Models.ViewModels;
 
 namespace TaskPlannerProject.Controllers;
 
 public class AuthenticateController: Controller
 {
+    private readonly DbContext _db;
+
+    public AuthenticateController(DbContext db)
+    {
+        _db = db;
+    }
 
     public async Task<IActionResult> Login()
     {
         return View(); 
+    }
+
+    public async Task<IActionResult> Login(AuthViewModel model)
+    {
+        
     }
 }
--- TaskPlannerProject/Program.cs
+++ TaskPlannerProject/Program.cs
@@ -45,6 +45,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Authenticate}/{action=Login}/{id?}");
 
 app.Run();
